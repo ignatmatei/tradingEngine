c3f2dec6b17028f8cf1a6383c36b6a03e3a2901a
matei <mateiignat03@gmail.com>
2025-03-14 17:32:21 +0200

    finished tbhe dataframe for the net

--- src/main.py
+++ src/main.py
@@ -1,5 +1,12 @@
+import numpy as np
 import pandas as pd
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense
 
-df = pd.read_excel('../data/trades20250128.xlsx', engine='openpyxl', sheet_name = 0 , skiprows= 7, usecols = [7,8,9,10,11,12])
+i= 28
+df = pd.read_excel('../data/trades202501' + str(i) + '.xlsx', engine='openpyxl', sheet_name = 0 , skiprows= 7, usecols = [7,8,9,10,11,12])
 
-print(df)
+#put all the stocks of the df in a dictionary
+stocks = df['Indici / Indices']
+
+print(stocks)
